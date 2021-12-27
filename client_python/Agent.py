623fafd02fb2ed8337406f2d954fe6baa4b367f9
ShauliTaragin <shaulitaragin@gmail.com>
2021-12-27 19:32:53 +0200

    added pokemon class

--- client_python/Agent.py
+++ client_python/Agent.py
@@ -7,4 +7,11 @@ class Agent:
         dest = 0
         speed = 0.0
         pos = 0 #this is position in geo location
+    """
+    if we want to change agent according to json file we read we can do that here.
+    """
+    def update_from_json(self, json_file:str):
+        pass
 
+    def onNOde(self)->bool:
+        pass
