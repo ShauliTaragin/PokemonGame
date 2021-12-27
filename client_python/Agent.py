4adc2f31b9c846a736f70668eb5f2abc9ea96241
ShauliTaragin <shaulitaragin@gmail.com>
2021-12-27 18:57:19 +0200

    added agent class

---
+++ client_python/Agent.py
@@ -0,0 +1,10 @@
+
+class Agent:
+    def __init__(self , id:int):
+        self.id = id
+        value = 0
+        src = 0
+        dest = 0
+        speed = 0.0
+        pos = 0 #this is position in geo location
+
