94e6d6751081c20577f7e00294512ffcc93f65a6
Ido Bar <ido1403@gmail.com>
2021-12-30 14:29:45 +0200

    added functions to edge node and geolocation

--- api/Edge.py
+++ api/Edge.py
@@ -1,7 +1,20 @@
+from api.GeoLocation import GeoLocation
+
+
 class Edge:
-    def __init__(self , src: int, dst: int , weight:int ):
+    def __init__(self, src: int, dst: int, weight: int, src_location: GeoLocation, dst_location: GeoLocation):
         self.src = src
         self.dst = dst
         self.weight = weight
         self.edge_type = src - dst
+        self.src_location = src_location
+        self.dst_location = dst_location
+        self.m, self.b = self.line_equation()[0], self.line_equation()[1]
 
+    def line_equation(self) -> tuple:
+        line_m = (self.src_location.x - self.dst_location.x) / (self.src_location.y - self.dst_location.y)
+        line_b = (self.src_location.y - line_m * self.src_location.x)
+        return line_m, line_b
+
+    def get_point_on_edge(self, x: float) -> float:
+        return self.m * x + self.b
--- api/GeoLocation.py
+++ api/GeoLocation.py
@@ -1,5 +1,11 @@
+import math
+
+
 class GeoLocation:
-    def __init__(self, x:float, y:float, z:float):
+    def __init__(self, x: float, y: float, z: float):
         self.x = x
         self.y = y
         self.z = z
+
+    def rang_to_other(self, other_location: tuple):
+        return math.sqrt((self.x - other_location[0]) ** 2 + (self.y - other_location[1]) ** 2)
